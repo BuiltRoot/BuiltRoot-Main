bac9297d705e055df53a59ebd691e8fd222c7b9e
BuiltRootFounder <founder@builtroot.com>
2025-08-19 10:43:50 -0700

    chore(config): add Prettier config

---
+++ packages/config/prettier.cjs
@@ -0,0 +1,11 @@
+/**
+ * Base Prettier config for BuiltRoot projects.
+ * Extend this in each app/package:  `@builtroot/config/prettier.cjs`
+ */
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  printWidth: 80,
+  tabWidth: 2
+};
